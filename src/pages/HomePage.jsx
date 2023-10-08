f90cca7162934dfbdca0d87b24b9c9bbe9ecaac0
hoontier <huntersutcliffe@hotmail.com>
2023-10-08 16:22:52 -0400

    home page skeleton

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,19 @@
 // HomePage.jsx
-
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import styles from "../styles/HomePage.module.css"
 import { Navbar } from '../components/Navbar';
 
 export const HomePage = () => {
     return (
-        <div className={styles['home-body']}>
-            <div className={styles['nav-bar']}>
-                <Navbar />
-            </div>
-            <div className={styles['filters']}>
-            </div>
-            <div className={styles['main']}>
+        <div className={styles['flex-container']}>
+            <div className={styles['home-body']}>
+                <div className={styles['nav-bar']}>
+                    <Navbar />
+                </div>
+                <div className={styles['filters']}>
+                </div>
+                <div className={styles['main']}>
+                </div>
             </div>
         </div>
     )
