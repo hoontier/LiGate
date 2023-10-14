d0dd7bdb0eb575e7ee614835837c89e3fbd57d87
hoontier <huntersutcliffe@hotmail.com>
2023-10-13 23:21:15 -0400

    filters and majors csv

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,8 +1,40 @@
 // Filters.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/Filters.module.css';
+import collegesAndDepartments from '../../collegesAndDepartments.json';
+import Select from 'react-select';
 
 export const Filters = () => {
+    const [colleges, setColleges] = useState([]);
+    const [selectedCollege, setSelectedCollege] = useState(null);
+    const [departments, setDepartments] = useState([]);
+    const [selectedDepartment, setSelectedDepartment] = useState(null);
+
+    useEffect(() => {
+        // Transform the college data to be used in React-Select
+        const collegeOptions = collegesAndDepartments.colleges.map(college => ({
+            value: college.name,
+            label: college.name
+        }));
+        setColleges(collegeOptions);
+    }, []);
+
+    useEffect(() => {
+        // If a college is selected, filter its departments
+        if (selectedCollege) {
+            const selected = collegesAndDepartments.colleges.find(
+                college => college.name === selectedCollege.value
+            );
+            const departmentOptions = selected.departments.map(department => ({
+                value: department,
+                label: department
+            }));
+            setDepartments(departmentOptions);
+        } else {
+            setDepartments([]);
+        }
+    }, [selectedCollege]);
+
     return (
         <div className={styles['filters-container']}>
             <h2 className={styles['filters-header']}>Filters</h2>
@@ -14,15 +46,28 @@ export const Filters = () => {
             </div>
             <div className={styles['filter-group']}>
                 <label className={styles['filter-label']} htmlFor="college">College</label>
-                <select className={styles['filter-select']} id="college" name="college">
-                    {/* Add options here */}
-                </select>
+                <Select
+                    id="college"
+                    name="college"
+                    options={colleges}
+                    value={selectedCollege}
+                    onChange={setSelectedCollege}
+                    isClearable
+                    placeholder="Select a College"
+                />
             </div>
             <div className={styles['filter-group']}>
                 <label className={styles['filter-label']} htmlFor="department">Department</label>
-                <select className={styles['filter-select']} id="department" name="department">
-                    {/* Add options here */}
-                </select>
+                <Select
+                    id="department"
+                    name="department"
+                    options={departments}
+                    value={selectedDepartment}
+                    onChange={setSelectedDepartment}
+                    isClearable
+                    placeholder="Select a Department"
+                    isDisabled={departments.length === 0}
+                />
             </div>
             <div className={styles['filter-group']}>
                 <label className={styles['filter-label']} htmlFor="min-gpa">Minimum GPA</label>
